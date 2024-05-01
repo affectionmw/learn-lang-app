722682012d6534f197bd1b24c59c13cfd29b1e39
affection <frankpearce90@gmail.com>
2024-05-01 20:52:05 +0300

    LLA-13 add lesson header

---
+++ app/lesson/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { getLesson, getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
+import { Quiz } from "@/app/lesson/quiz";
+
+const LessonPage = async () => {
+    const lessonReq = getLesson();
+    const userProgressReq = getUserProgress();
+
+    const [lesson, userProgress] = await Promise.all([lessonReq, userProgressReq]);
+
+    if (!lesson || !userProgress) {
+        redirect("/learn");
+    }
+
+    const initialPercentage =
+        (lesson.challenges.filter((challenge) => challenge.completed).length / lesson.challenges.length) * 100;
+
+    return (
+        <Quiz
+            initialLessonId={lesson.id}
+            initialLessonChallenges={lesson.challenges}
+            initialHearts={userProgress.hearts}
+            initialPercentage={initialPercentage}
+            userSubscription={null}
+        />
+    );
+};
+
+export default LessonPage;
